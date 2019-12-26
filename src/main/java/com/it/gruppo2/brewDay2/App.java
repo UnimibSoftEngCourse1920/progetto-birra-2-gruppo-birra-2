f523e19e72736aad14627cc226b5cecfdb837b51
arusso96 <56863798+arusso96@users.noreply.github.com>
2019-12-26 11:33:01 +0100

    branch master with empty maven project

---
+++ src/main/java/com/it/gruppo2/brewDay2/App.java
@@ -0,0 +1,13 @@
+package com.it.gruppo2.brewDay2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
