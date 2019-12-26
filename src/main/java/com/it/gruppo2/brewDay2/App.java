876af392cc202cc495226863bd37c10db5e1502f
arusso96 <56863798+arusso96@users.noreply.github.com>
2019-12-26 12:08:37 +0100

    Initialized connection to brewdayDB

--- src/main/java/com/it/gruppo2/brewDay2/App.java
+++ src/main/java/com/it/gruppo2/brewDay2/App.java
@@ -1,13 +1,34 @@
 package com.it.gruppo2.brewDay2;
 
+import java.sql.*;
+
 /**
- * Hello world!
+ * Connection to BrewDayDB
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Connection connection = null;
+    		try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				String dbpathString = "jdbc:mysql://localhost:3306/brewdaydb";
+				String usernameString = "root";
+				String pwdString = "";
+				connection = DriverManager.getConnection(dbpathString, usernameString, pwdString);
+				if(!connection.isClosed())
+					System.out.println("Successfully connected to brewDayDB...");
+    		} catch (Exception e) {
+				System.err.println("Excpetion: "+ e.getMessage());
+			}
+    		finally {
+				try {
+					if(connection != null)
+						connection.close();
+				} catch (SQLException e) {
+					System.err.println("Excpetion: "+ e.getMessage());
+				}
+			}
     }
 }
