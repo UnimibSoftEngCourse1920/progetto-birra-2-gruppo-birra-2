e442baa735dbd005654a2a16e26fcbac0a9400ca
arusso96 <56863798+arusso96@users.noreply.github.com>
2019-12-27 16:51:11 +0100

    created a personalized communication with any db and not a preconfigured db

--- src/main/java/com/it/gruppo2/brewDay2/App.java
+++ src/main/java/com/it/gruppo2/brewDay2/App.java
@@ -1,34 +1,19 @@
 package com.it.gruppo2.brewDay2;
 
-import java.sql.*;
+import java.io.IOException;
+
+import com.it.gruppo2.connectionDB.connectionDB;
 
 /**
- * Connection to BrewDayDB
+ * BrewDayApp
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-    	Connection connection = null;
-    		try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-				String dbpathString = "jdbc:mysql://localhost:3306/brewdaydb";
-				String usernameString = "root";
-				String pwdString = "";
-				connection = DriverManager.getConnection(dbpathString, usernameString, pwdString);
-				if(!connection.isClosed())
-					System.out.println("Successfully connected to brewDayDB...");
-    		} catch (Exception e) {
-				System.err.println("Excpetion: "+ e.getMessage());
-			}
-    		finally {
-				try {
-					if(connection != null)
-						connection.close();
-				} catch (SQLException e) {
-					System.err.println("Excpetion: "+ e.getMessage());
-				}
-			}
+    	connectionDB connectionDB = new connectionDB();
+    	connectionDB.setDBCredential();
+    	connectionDB.createDBConnection();
     }
 }
