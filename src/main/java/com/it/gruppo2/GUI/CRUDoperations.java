768111310f3bae05aeba1c958de89ab2c8e344c1
arusso96 <56863798+arusso96@users.noreply.github.com>
2020-01-28 01:41:43 +0100

    Added CRUD per ricetta

--- src/main/java/com/it/gruppo2/GUI/CRUDoperations.java
+++ src/main/java/com/it/gruppo2/GUI/CRUDoperations.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 
 import com.it.gruppo2.brewDay2.Birraio;
@@ -14,11 +13,19 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 
 public class CRUDoperations {
 
 	private JFrame frame;
+	private JTextField txtNome;
+	private JTextField txtQta;
+	JComboBox<String> comboBox = new JComboBox<String>();
 
 	/**
 	 * Launch the application.
@@ -53,7 +60,7 @@ public class CRUDoperations {
 	 */
 	private void initialize(final Connection connection, final Birraio birraio, String operation) throws SQLException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 840, 432);
+		frame.setBounds(100, 100, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -71,7 +78,77 @@ public class CRUDoperations {
 		menuBar.add(mntmIndietro);
 		frame.getContentPane().setLayout(null);
 		
-		if(operation == "showIngr") {
+		if(operation == "addIngr") {
+			JLabel lblNomeIngrediente = new JLabel("Nome Ingrediente");
+			lblNomeIngrediente.setBounds(77, 64, 182, 20);
+			frame.getContentPane().add(lblNomeIngrediente);
+			
+			txtNome = new JTextField();
+			txtNome.setBounds(305, 61, 267, 26);
+			frame.getContentPane().add(txtNome);
+			txtNome.setColumns(10);
+			
+			JLabel lblTipo = new JLabel("Tipo");
+			lblTipo.setBounds(77, 128, 182, 20);
+			frame.getContentPane().add(lblTipo);
+			
+			String[] tipo = {"acqua", "malto", "luppolo", "lievito", "addittivi","zucchero"};
+			comboBox = new JComboBox<String>(tipo);
+			comboBox.setSelectedIndex(1);
+			comboBox.setBounds(305, 125, 267, 26);
+			frame.getContentPane().add(comboBox);
+			
+			txtQta = new JTextField();
+			txtQta.setBounds(305, 196, 267, 26);
+			frame.getContentPane().add(txtQta);
+			txtQta.setColumns(10);
+			
+			JLabel lblQuantit = new JLabel("Quantità");
+			lblQuantit.setBounds(77, 199, 69, 20);
+			frame.getContentPane().add(lblQuantit);
+			
+			JButton btnCreaIngrediente = new JButton("Crea Ingrediente");
+			btnCreaIngrediente.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent e) {
+					Statement stmt;
+					try {
+						stmt = connection.createStatement();
+						//verifica che non esista un altro ingrediente uguale
+						String sql = "SELECT ingrediente.nome AS nome FROM dispensa INNER JOIN ingrediente ON ingrediente.id_ingrediente = dispensa.id_ingrediente WHERE ingrediente.nome = '"+ txtNome.getText() +"' AND ingrediente.tipo = '"+ comboBox.getSelectedItem() +"' AND dispensa.id_birraio = '" + birraio.getId_birraio() + "'";
+						ResultSet rs = stmt.executeQuery(sql);
+
+						if(rs.next())
+						{
+							System.out.println("ERROR: ALREADY EXISTS!");	
+						}
+						else {
+							System.out.println("Insert new ingrediente into db...");
+							sql = "INSERT INTO ingrediente (nome,tipo)" +
+					                   "VALUES ('"+txtNome.getText()+"','"+comboBox.getSelectedItem()+"')";
+							stmt.executeUpdate(sql);
+							sql = "SELECT MAX(ingrediente.id_ingrediente) AS id FROM ingrediente";
+							rs = stmt.executeQuery(sql);
+							rs.next();
+							System.out.println("Insert quantity into db...");
+							sql = "INSERT INTO dispensa (qta, id_ingrediente, id_birraio)" +
+					                   "VALUES ('"+txtQta.getText()+"','"+rs.getInt("id")+"','"+birraio.getId_birraio()+"')";
+							stmt.executeUpdate(sql);
+							BrewDayMenu bDayMenu = new BrewDayMenu(connection, birraio);
+							bDayMenu.invokeGUI(connection, birraio);
+							frame.dispose();
+						}
+						rs.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+			btnCreaIngrediente.setBounds(600, 124, 192, 29);
+			frame.getContentPane().add(btnCreaIngrediente);
+		}
+		if(operation == "showIngr") 
+		{
 			Statement stmt;
 			stmt = connection.createStatement();
 			
@@ -97,6 +174,5 @@ public class CRUDoperations {
 			frame.getContentPane().add(listCapienza);
 			rs.close();
 		}
-		
 	}
 }
